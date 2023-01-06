ea15207e812e2a46725254738692201ea946fe0c
professorK <jeffpk@gmail.com>
2023-01-06 17:00:14 -0500

    Compiling and linking with UE5 must be butil wtih MSVC tools 142+ (checked with 143)  MUSt be built for 64 bit

--- ONNXBuilder/ONNXBuilder.h
+++ ONNXBuilder/ONNXBuilder.h
@@ -3,15 +3,15 @@
 extern "C" {
 // this is a super structure to hide the details of the model struct
     
-    struct OpaqueModel
+     struct OpaqueModel
     {
     
     };
     
-    OpaqueModel& MakeMLP(int input_layer,int hidden_layer,int hidden_layer_count,int output_layer,
+    extern  OpaqueModel& MakeMLP(int input_layer,int hidden_layer,int hidden_layer_count,int output_layer,
                 float bias);
-    int  GetWeightCount(OpaqueModel& model);
-    float* ExtractWeights(OpaqueModel& model);
-    void  SetWeights(OpaqueModel& model, float* weights);
+    extern int  GetWeightCount(OpaqueModel& model);
+    extern float* ExtractWeights(OpaqueModel& model);
+    extern void  SetWeights(OpaqueModel& model, float* weights);
     
 }
